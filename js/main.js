f93b6769ef455e621394ff1694ea0f082e8a2d4a
Parker <parker1202002@gmail.com>
2023-07-28 04:22:35 +0500

    add other

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const ToTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => 
+{
+    if (window.pageYOffset > 100) 
+    {
+        ToTop.classList.add("active");
+    } 
+    else 
+        {
+            ToTop.classList.remove("active");
+        }
+})
