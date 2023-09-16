8973c84d1de69e7002a6aa0ae398f026189bb71e
Parker <parker1202002@gmail.com>
2023-09-16 17:37:35 +0500

    fix

--- uparrow.js
+++ uparrow.js
@@ -7,3 +7,8 @@ window.addEventListener("scroll", () => {
         ToTop.classList.remove("active");
     }
 })
+screen.addEventListener("orientationchange", function () {
+    console.log("The orientation of the screen is: " + screen.orientation);
+  });
+  screen.lockOrientation("landscape");
+  "orientation": "portrait"
