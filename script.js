afb624a725e973ccdf64172bed27b1a3933ac0a5
Parker <parker1202002@gmail.com>
2023-09-30 14:11:49 +0500

    slider

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		dots: true,
+		infinite: true,
+		speed: 1000,
+		slidesToShow: 1,
+		adaptiveHeight: true,
+		easing:'liner',
+		infinite:true,
+		autoplay: true,
+		sautoplaySpeed: 1500,
+		/*pauseOnFocus:true,
+		pauseOnHover:true,
+		pauseOnDotsHover:true,*/
+	});
+});
