70dc70f258e128047adf7ba01e73f7250ad7f9c1
Parker <parker1202002@gmail.com>
2024-06-03 21:44:59 +0500

    fix

---
+++ redirect.js
@@ -0,0 +1,18 @@
+var f = document.title;
+var u = window.location.pathname.replace(/.*website\//,'');
+var au = document.querySelector("a[href='"+u+"']");
+var af = document.querySelector("a[href='"+f+"']");
+au.className += ' tu';
+af.className += ' tf';
+au.parentNode.previousElementSibling.className += ' a';
+af.parentNode.nextElementSibling.className += ' a';
+var p = window.location.protocol;
+var m = document.getElementById('message');
+if (p == 'http:') {
+    m.className += ' warn';
+    m.innerHTML = '<i class="fa fa-exclamation-triangle" aria-hidden="true"></i> INSECURE CONNECTION';
+}
+if (p == 'https:') {
+    m.className += ' ok';
+    m.innerHTML = '<i class="fa fa-lock" aria-hidden="true"></i> HTTPS';
+}
--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	$('.slider').slick({
 		dots: true,
 		infinite: true,
 		speed: 1000,
 		slidesToShow: 1,
 		adaptiveHeight: true,
-		easing:'liner',
-		infinite:true,
+		easing: 'liner',
+		infinite: true,
 		autoplay: true,
 		sautoplaySpeed: 1500,
 		/*pauseOnFocus:true,
